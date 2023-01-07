4f3b8ae46d9b1508e6e395a8831c1d0ce0772b89
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2023-01-07 20:27:47 +0500

    Initialize project using Create React App

---
+++ src/pages/Course.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Blog from '../Components/Blog/Blog'
+import Footer from '../Components/Footer/Footer'
+
+const Course = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Blog/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Course
