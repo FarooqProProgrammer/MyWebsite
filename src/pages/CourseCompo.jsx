3e713d51c70f9b13e57e7e548aee26e0a33d5e7b
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2023-01-08 11:25:22 +0500

    Update CourseCompo.jsx

--- src/pages/CourseCompo.jsx
+++ src/pages/CourseCompo.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer/Footer'
+import Student from '../Components/Student'
 
 const CourseCompo = () => {
   return (
     <div>
         <Navbar/>
+        <Student/>
         <Footer/>
     </div>
   )
